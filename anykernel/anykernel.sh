3731866d4cdd109dc804d7e76c28f9f6fbb0073e
fredslbn <138670465+fredslbn@users.noreply.github.com>
2026-09-25 13:41:38 +0700

    Update anykernel.sh

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -34,7 +34,7 @@ no_magisk_check=1
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
 
-ui_print "Flashing Capybara CLO Kernel 6.2..."
+ui_print "Flashing CLO Kernel 6.2..."
 
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
